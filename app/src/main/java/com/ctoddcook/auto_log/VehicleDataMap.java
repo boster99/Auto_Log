1e4ec616d7d1b3ac338ac20a48bb2b7f01ee2efe
boster99 <ctodd@ctoddcook.com>
2016-05-10 07:54:51 -0500

    Added SELECT sql; added column number constants

--- app/src/main/java/com/ctoddcook/auto_log/VehicleDataMap.java
+++ app/src/main/java/com/ctoddcook/auto_log/VehicleDataMap.java
@@ -14,19 +14,26 @@ import android.provider.BaseColumns;
  */
 public class VehicleDataMap extends AutoLogDataMap implements BaseColumns {
     public static final String TABLE_NAME;
-    public static final String COLUMN_NAME_ID;
     public static final String COLUMN_NAME_NAME;
     public static final String COLUMN_NAME_YEAR;
     public static final String COLUMN_NAME_COLOR;
     public static final String COLUMN_NAME_MODEL;
     public static final String COLUMN_NAME_VIN;
     public static final String COLUMN_NAME_LICENSE_PLATE;
-    public static final String COLUMN_NAME_LAST_UPDATED;   // When the record was saved
+    public static final String COLUMN_NAME_LAST_UPDATED;
 
+    // These column numbers must match the order in the CREATE TABLE and SELECT statements
+    public static final int COLUMN_NBR_ID = 0;
+    public static final int COLUMN_NBR_NAME = 1;
+    public static final int COLUMN_NBR_YEAR = 2;
+    public static final int COLUMN_NBR_COLOR = 3;
+    public static final int COLUMN_NBR_MODEL = 4;
+    public static final int COLUMN_NBR_VIN = 5;
+    public static final int COLUMN_NBR_LICENSE_PLATE = 6;
+    public static final int COLUMN_NBR_LAST_UPDATED = 7;
 
     static {
         TABLE_NAME = "vehicle";
-        COLUMN_NAME_ID = "vehicle_id";
         COLUMN_NAME_NAME = "vehicle_name";
         COLUMN_NAME_YEAR = "vehicle_year";
         COLUMN_NAME_COLOR = "vehicle_color";
@@ -47,9 +54,13 @@ public class VehicleDataMap extends AutoLogDataMap implements BaseColumns {
                     COLUMN_NAME_LICENSE_PLATE + STRING_TYPE + NOT_NULL + COMMA_SEP +
                     COLUMN_NAME_LAST_UPDATED + DATETIME_TYPE + " )";
 
-    public static final String SQL_DROP_VEHICLE =
+    public static final String SQL_DROP_TABLE =
             DROP_TABLE_PHRASE + TABLE_NAME;
 
+    public static final String SQL_SELECT_ALL =
+            SELECT_PHRASE + "*" + FROM_PHRASE +
+                    TABLE_NAME + ORDER_BY_PHRASE + _ID;
+    
     public static boolean tableExists(SQLiteDatabase db) {
         return tableExists(db, TABLE_NAME);
     }
