7b338836c413ec71ac0f25c63eba90dab0fc3728
boster99 <ctodd@ctoddcook.com>
2016-05-11 15:46:29 -0500

    Initial commit for adding AddVehicle activity

---
+++ app/src/main/java/com/ctoddcook/auto_log/AddVehicleActivity.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 C. Todd Cook. All rights reserved.
+ */
+
+package com.ctoddcook.auto_log;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AddVehicleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_vehicle);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
