07def5d75355f4e29d2ce862ad7dbdf94a877cad
boster99 <ctodd@ctoddcook.com>
2016-05-14 11:33:46 -0500

    Refactor roundDouble() to just round(), and overload it to include a version for float values

--- app/src/main/java/com/ctoddcook/tools/CTools.java
+++ app/src/main/java/com/ctoddcook/tools/CTools.java
@@ -21,13 +21,27 @@ public class CTools {
      * @param places The scale for rounding (i.e., the significant digits)
      * @return A new double value rounded as indicated
      */
-    public static double roundDouble(double value, int places) {
-        if (value == 0)
-            return 0;
+    public static double round(double value, int places) {
+        if (value == 0) return 0;
 
         BigDecimal bd = new BigDecimal(value);
         bd = bd.setScale(places, BigDecimal.ROUND_HALF_UP);
         return bd.doubleValue();
+    }
+
+
+    /**
+     * This takes a float value, and rounds it to the indicated scale, returning a new
+     * float.
+     * @param value The value to be rounded
+     * @param places The scale for rounding (i.e., the significant digits)
+     * @return A new float value rounded as indicated
+     */
+    public static float round(float value, int places) {
+        if (value == 0) return 0;
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
     }
 }
 
