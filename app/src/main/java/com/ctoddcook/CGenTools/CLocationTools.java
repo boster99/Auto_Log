52bdeb9cfec6710acf85548bc7d5702778adb875
boster99 <ctodd@ctoddcook.com>
2016-05-18 07:58:07 -0500

    Add methods for determining if one location is better than another

--- app/src/main/java/com/ctoddcook/CGenTools/CLocationTools.java
+++ app/src/main/java/com/ctoddcook/CGenTools/CLocationTools.java
@@ -23,6 +23,8 @@ import java.util.List;
  */
 public class CLocationTools {
   private static final String TAG = "CLocationTools";
+  private static final int TWO_MINUTES = 1000 * 60 * 2;
+
 
 
   /**
@@ -84,15 +86,15 @@ public class CLocationTools {
 
   /**
    * Returns the longitude and latitude from a given Location object, in the form
-   * <code>[longitude].[latitude]</code>. For example, "-122.2,294.8".
+   * <code>[latitude].[longitude]</code>. For example, "-122.2,294.8".
    * @param loc the source location object
    * @return the longitude and latitude, or "---" if the source is null
    */
-  public static String getLongitudeAndLatitude(Location loc) {
+  public static String getLatitudeAndLongitude(Location loc) {
     String longLat;
 
     if (loc != null) {
-      longLat = loc.getLongitude() + "," + loc.getLatitude();
+      longLat = loc.getLatitude() + "," + loc.getLongitude();
     }
     else
       longLat = "---";
@@ -100,5 +102,69 @@ public class CLocationTools {
     return longLat;
   }
 
+  /** Determines whether one Location reading is better than the current Location fix.
+   * <p>
+   * Taken directly from Android API Guides (https://developer.android
+   * .com/guide/topics/location/strategies.html)
+   * @param location  The new Location that you want to evaluate
+   * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+   */
+  protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+    if (currentBestLocation == null) {
+      // A new location is always better than no location
+      return true;
+    }
+
+    // Check whether the new location fix is newer or older
+    long timeDelta = location.getTime() - currentBestLocation.getTime();
+    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+    boolean isNewer = timeDelta > 0;
+
+    // If it's been more than two minutes since the current location, use the new location
+    // because the user has likely moved
+    if (isSignificantlyNewer) {
+      return true;
+      // If the new location is more than two minutes older, it must be worse
+    } else if (isSignificantlyOlder) {
+      return false;
+    }
+
+    // Check whether the new location fix is more or less accurate
+    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+    boolean isLessAccurate = accuracyDelta > 0;
+    boolean isMoreAccurate = accuracyDelta < 0;
+    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+    // Check if the old and new location are from the same provider
+    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+        currentBestLocation.getProvider());
+
+    // Determine location quality using a combination of timeliness and accuracy
+    if (isMoreAccurate) {
+      return true;
+    } else if (isNewer && !isLessAccurate) {
+      return true;
+    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Checks whether two providers are the same
+   * <p>
+   * Taken directly from Android API Guides (https://developer.android
+   * .com/guide/topics/location/strategies.html)
+   * @param provider1 first provider
+   * @param provider2 second provider
+   * @return true if they are the same, including if they are both <code>null</code>
+   */
+  private boolean isSameProvider(String provider1, String provider2) {
+    if (provider1 == null) {
+      return provider2 == null;
+    }
+    return provider1.equals(provider2);
+  }
 
 }
