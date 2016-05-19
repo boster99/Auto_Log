7e4a455cbb5ee46f5ad9054214e49afe097d37ca
boster99 <ctodd@ctoddcook.com>
2016-05-18 20:22:13 -0500

    Refactor: Move to different package

--- app/src/main/java/com/ctoddcook/CGenTools/DataHolder.java
+++ app/src/main/java/com/ctoddcook/CGenTools/DataHolder.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 C. Todd Cook. All rights reserved.
  */
 
-package com.ctoddcook.auto_log;
+package com.ctoddcook.CGenTools;
 
 import java.util.Calendar;
 import java.util.Date;
