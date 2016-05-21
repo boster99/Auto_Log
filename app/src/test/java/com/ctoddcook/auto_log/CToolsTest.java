1c9c628151a6c07aaf5792355ad7685dc2a1ec80
boster99 <ctodd@ctoddcook.com>
2016-05-20 21:26:44 -0500

    Move to new package

--- app/src/test/java/com/ctoddcook/auto_log/CToolsTest.java
+++ app/src/test/java/com/ctoddcook/auto_log/CToolsTest.java
@@ -8,8 +8,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
-import static com.ctoddcook.tools.CTools.roundDouble;
-import static org.junit.Assert.*;
+import static com.ctoddcook.CGenTools.CTools.round;
 
 /**
  * Created by C. Todd Cook on 4/22/2016.
@@ -18,19 +17,19 @@ import static org.junit.Assert.*;
 public class CToolsTest {
 
     /**
-     * Test CTools.roundDouble with the same value, but different scales.
+     * Test CTools.round with the same value, but different scales.
      * @throws Exception
      */
     @Test
     public void testRoundDouble() throws Exception {
         double val = 156.23456;
 
-        Assert.assertEquals(156.2346, roundDouble(val, 4));
-        Assert.assertEquals(156.235, roundDouble(val, 3));
-        Assert.assertEquals(156.23, roundDouble(val, 2));
-        Assert.assertEquals(156.2, roundDouble(val, 1));
-        Assert.assertEquals(156.0, roundDouble(val, 0));
-        Assert.assertEquals(160.0, roundDouble(val, -1));
+        Assert.assertEquals(156.2346, round(val, 4));
+        Assert.assertEquals(156.235, round(val, 3));
+        Assert.assertEquals(156.23, round(val, 2));
+        Assert.assertEquals(156.2, round(val, 1));
+        Assert.assertEquals(156.0, round(val, 0));
+        Assert.assertEquals(160.0, round(val, -1));
     }
 
 }
