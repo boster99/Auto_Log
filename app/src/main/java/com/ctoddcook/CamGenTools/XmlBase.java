a691b5c5971791a69aebd942c44664645a251e69
boster99 <ctodd@ctoddcook.com>
2016-06-28 13:52:11 -0500

    Major rewrite

--- app/src/main/java/com/ctoddcook/CamGenTools/XmlBase.java
+++ app/src/main/java/com/ctoddcook/CamGenTools/XmlBase.java
@@ -1,95 +1,79 @@
 package com.ctoddcook.CamGenTools;
 
+import java.io.IOException;
+
 /**
  * Base class for XML importer and exporter.
  *
  * @author <a href="mailto:konstantin.sobolev@gmail.com">Konstantin Sobolev</a>
  */
 public class XmlBase {
-  public static final String DATABASE_TAG = "database";
-  public static final String TABLE_TAG = "table";
-  public static final String NAME_ATTR = "name";
-  public static final String PK_ATTR = "pk";
-  public static final String ROW_TAG = "row";
-  public static final String COLUMN_TAG = "col";
+  protected static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+  protected static final String INDENT = "  ";
 
-  public static String escape(final String str) {
-    if (str == null || str.length() == 0)
+
+  /**
+   * Takes an input string, and searches for any special characters which must be escaped in XML,
+   * and if it finds any, replaces them with the escape value.
+   *
+   * @param input A value which may require escaping
+   * @return A string with characters escaped where required
+   */
+  public static String encodeValue(String input) {
+    if (input == null || input.length() == 0)
       return "";
 
-    final StringBuilder buf = new StringBuilder();
-    final int len = str.length();
-    for (int i = 0; i < len; i++)
-      escapeChar(str.charAt(i), buf);
-    return buf.toString();
+    /*
+    In the case of ampersands, we want to replace them with &amp; UNLESS the existing ampersand
+    is already part of a escape sequence.
+     */
+    input = input.replaceAll("&(?!amp;)(?!lt;)(?!gt;)(?!quot;)(?!apos;)", "&amp;");
+
+    /*
+    The other replacements are more straight forward.
+     */
+    input = input.replaceAll("<", "&lt;");
+    input = input.replaceAll(">", "&gt;");
+    input = input.replaceAll("\"", "&quot;");
+    input = input.replaceAll("'", "&apos;");
+
+    return input;
   }
 
-  private static void escapeChar(final char c, final StringBuilder buf) {
-    switch (c) {
-      case '&':
-        buf.append("&amp;");
-        break;
-      case '<':
-        buf.append("&lt;");
-        break;
-      case '>':
-        buf.append("&gt;");
-        break;
-      case '"':
-        buf.append("&quot;");
-        break;
-      case '\'':
-        buf.append("&apos;");
-        break;
-      default:
-        buf.append(c);
-        break;
+  /**
+   * Forms an opening XML tag for the provided tag name. Optionally includes attribute-value pairs.
+   *
+   * @param name  The tag to be opened
+   * @param attrs Optional attribute name-value pairs. There must be an even number of these, with
+   *              the name of each attribute followed by its value.
+   * @return The formatted opening tag, including optional attribute names and values
+   * @throws IOException
+   */
+  protected String openTag(final String name, final String... attrs) throws IOException {
+    final StringBuilder tag = new StringBuilder();
+
+    tag.append('<').append(name);
+
+    if (attrs.length > 0) {
+      Assert.isTrue(attrs.length % 2 == 0, "After the tag parameter, parameters must be provided " +
+          "in 'name, value' pairs.");
+      for (int i = 0; i < attrs.length; i++) {
+        tag.append(' ').append(attrs[i++]).append("=\"").append(attrs[i]).append('"');
+      }
     }
+
+    tag.append('>');
+    return tag.toString();
   }
 
-  public static String unescape(final String str) {
-    if (str == null || str.length() == 0)
-      return "";
-
-    final StringBuilder buf = new StringBuilder();
-    final int len = str.length();
-    for (int i = 0; i < len; i++) {
-      final char c = str.charAt(i);
-      if (c == '&') {
-        final int pos = str.indexOf(";", i);
-        if (pos == -1) {
-          buf.append('&');
-        } else if (str.charAt(i + 1) == '#') {
-          final int val = Integer.parseInt(str.substring(i + 2, pos), 16);
-          buf.append((char) val);
-          i = pos;
-        } else {
-          final String substr = str.substring(i, pos + 1);
-          switch (substr) {
-            case "&amp;":
-              buf.append('&');
-              break;
-            case "&lt;":
-              buf.append('<');
-              break;
-            case "&gt;":
-              buf.append('>');
-              break;
-            case "&quot;":
-              buf.append('"');
-              break;
-            case "&apos;":
-              buf.append('\'');
-              break;
-            default:
-              buf.append(substr);
-              break;
-          }
-          i = pos;
-        }
-      } else
-        buf.append(c);
-    }
-    return buf.toString();
+  /**
+   * Forms a closing XML tag for the provided tag name.
+   *
+   * @param tag The tag to be closed
+   * @return A string containing the XML closing tag in braces
+   * @throws IOException
+   */
+  protected String closeTag(final String tag) throws IOException {
+    return "</" + tag + '>';
   }
 }
