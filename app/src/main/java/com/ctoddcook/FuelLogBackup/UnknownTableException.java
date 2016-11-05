89e9adace7b9410e38018b0747c6b6468ccda191
boster99 <ctodd@ctoddcook.com>
2016-11-05 11:17:22 -0500

    Initial commit

---
+++ app/src/main/java/com/ctoddcook/FuelLogBackup/UnknownTableException.java
@@ -0,0 +1,11 @@
+package com.ctoddcook.FuelLogBackup;
+
+/**
+ * Used when an unknown table is found in encoding or decoding.
+ * <p>
+ * Created by C. Todd Cook on 11/5/2016.<br>
+ * ctodd@ctoddcook.com
+ */
+
+class UnknownTableException extends Exception {
+}
